84f78318330537514660a5fad906fb47ba89ad8f
Shubham Singh Sendhav <sendhavshubham38@gmail.com>
2025-01-04 14:32:13 +0530

    Added react hot toaster

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 
+import { Router } from 'react-router-dom';
 import './App.css';
 
 function App() {
   
 
   return (
-    <> 
+    // <Router> 
+    // <h1>radhe radhe</h1>
+    // </Router>
     <h1>radhe radhe</h1>
-    </>
   )
 }
 
