3d4dcf3c5e421f10f781b4091516778c7a8d40f2
Shubham Singh Sendhav <sendhavshubham38@gmail.com>
2025-01-05 08:52:48 +0530

    starting adding basic redux

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,19 @@
 import { StrictMode } from 'react'
-import { createRoot ,ReactDOM} from 'react-dom/client'
+import { createRoot } from 'react-dom/client'
+import { Toaster } from 'react-hot-toast'
 import { BrowserRouter } from 'react-router-dom'
 
 import './index.css'
 import App from './App.jsx'
+import { Toaster } from 'react-hot-toast'
+import { Provider } from 'react-redux'
 
 createRoot(document.getElementById('root')).render(
-
+<Provider>
    <BrowserRouter>
     <App />
+    <Toaster/>
    </BrowserRouter>
+   </Provider>
 
 )
